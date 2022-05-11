74dcd4033408e92f1f5c6b21bc931c53a8d663a0
Paddy-Nimrod <n.ambetsa21@gmail.com>
2022-05-11 15:48:58 +0300

    created the student class and data repo. first commit

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return Args ->{
+            Student nimrod = new Student("nimrod","paul","n.ambetsa21@gmail.com",21);
+            studentRepository.save(nimrod);
+        };
     }
 
 }
